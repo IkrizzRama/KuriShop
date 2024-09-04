f02cf7f011d4fc5fdeb2b1233edee98cfe264b73
Rama <rizkyramadhani.ixh@gmail.com>
2024-09-04 22:42:51 +0700

    Membuat model pada aplikasi main dengan nama Product dan memiliki atribut wajib

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.IntegerField(auto_now_add=True)
+    description = models.TextField()
+
+
 # Create your models here.
