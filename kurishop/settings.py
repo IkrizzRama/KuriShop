f88482c1f125995e67001d97ac487cf6a7504a94
RizkyRama <147387755+IkrizzRama@users.noreply.github.com>
2024-09-04 22:07:27 +0700

    Allowed Host Change

--- kurishop/settings.py
+++ kurishop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7qt#ul!^w_01pe$wdi(07n58(4iq+-y*8^x&eg)apo#8b&ylds
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "muhammad-rizky36-kurishop.pbp.cs.ui.ac.id"]
 
 
 # Application definition
