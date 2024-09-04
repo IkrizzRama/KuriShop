2cc8782f80b53053b81e1ecb64214aec5b5cfaa7
Rama <rizkyramadhani.ixh@gmail.com>
2024-09-04 22:50:44 +0700

    Membuat sebuah routing pada urls.py dan memetakan fungsi pada views.py

--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+def show_main(request):
+    context = {
+        'name' : 'Pistol Air',
+        'price': 'Rp.100.000',
+        'description': 'Menembakan Air'
+    }
+
+    return render(request, "main.html", context)
 # Create your views here.
