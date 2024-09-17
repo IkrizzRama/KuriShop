4d4e4fb31317401d711c14fc37cf3f89e048afe9
Rama <rizkyramadhani.ixh@gmail.com>
2024-09-17 13:24:24 +0700

    Add show xml and json by id

--- main/views.py
+++ main/views.py
@@ -39,3 +39,11 @@ def show_json(request):
 def show_json(request):
     data = objectEntry.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = objectEntry.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = objectEntry.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
